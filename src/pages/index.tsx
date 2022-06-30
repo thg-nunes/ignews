433dc37a9a26fb5229bf31e1dd53887f9ecd9a22
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-30 12:41:15 -0300

    feature: adiciona icons da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div>Ig News</div>
+    <>
+      <Head>
+        <title>Inicio | Ig News</title>
+      </Head>
+    </>
   )
 }
 
