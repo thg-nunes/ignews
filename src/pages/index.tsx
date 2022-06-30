2cc24923bc8adb5a97155bd584afdbe0d63ac9b2
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-30 14:37:14 -0300

    eature: cria home-page da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Inicio | Ig News</title>
       </Head>
+
+      <main className={styles.homeContainer}>
+        <section className={styles.homeContent}>
+          <span>👏Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world</h1>
+          <p>Get acess to all the publications <br /> <span>for $9,90 month</span></p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
