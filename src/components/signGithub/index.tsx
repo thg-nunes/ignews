46cb55f0c3ae33cbb76d110c6a982334482a4fa9
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-30 21:29:38 -0300

    feature: implementa funcao de login com github

--- src/components/signGithub/index.tsx
+++ src/components/signGithub/index.tsx
@@ -1,18 +1,20 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss'
 
 export const SignGithub = () => {
-  const isUserLoggedIn = false
+  const session = useSession()
 
-  return isUserLoggedIn ? (
+  return (session.data !== null && session.data !== undefined) ? (
     <button
       type="button"
       className={styles.signGithub}
+      onClick={() => signOut()}
     >
       <FaGithub color='#04D361' />
-      Sing in with GitHub
+      {session.data.user?.name}
       <FiX
         color='#737380'
         className={styles.closeButton}
@@ -22,6 +24,7 @@ export const SignGithub = () => {
     <button
       type="button"
       className={styles.signGithub}
+      onClick={() => signIn('github')}
     >
       <FaGithub
         color='#EBA417'
