405cc41344c6f6d9f6597cdfcf5220663be4a0a4
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-01 12:22:10 -0300

    feature: salva email do usuario no faunadb caso nao exista

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { fauna } from "../faunaDB"
+import { Collection, Create, Exists, If, Index, Match, Not, Casefold, Get } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
@@ -8,4 +10,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+
+  callbacks: {
+    async signIn({user}) {
+      const email = user.email !== null && user.email !== undefined ? user.email : ''
+
+      try {
+        await fauna.query(
+          If(
+            Not(
+              Exists(
+                Match(
+                  Index('user_by_email'),
+                  Casefold(email)
+                )
+              )
+            ),
+            Create(
+              Collection('users'),
+              {
+                data: { email }
+              }
+            ),
+            Get(
+              Match(
+                Index('user_by_email'),
+                Casefold(email)
+              )
+            )
+          ))
+          return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
