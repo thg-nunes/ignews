14cb318e73725e9621227b88ea9bec6cfdd744fc
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-27 13:01:53 -0300

    test: usuario redirecionado se inscricao for ativa

--- src/components/subscribe-Button/subscribeButton.spec.tsx
+++ src/components/subscribe-Button/subscribeButton.spec.tsx
@@ -1,21 +1,23 @@
 import { render, screen, fireEvent } from "@testing-library/react"
-import { signIn } from "next-auth/react"
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 import { SubscribButton } from "."
 
-jest.mock('next-auth/react', () => {
-  return {
-    useSession() {
-      return {
-        data: null,
-        status: 'unauthenticated'
-      }
-    },
-    signIn: jest.fn()
-  }
-})
+jest.mock('next-auth/react')
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+  }),
+}))
 
 describe('SubscribeButton Component', () => {
   test('SubscribeButton renderiza corretamente', () => {
+
+    const useSessionMocked = jest.mocked(useSession)
+    useSessionMocked.mockReturnValueOnce({
+      data: null,
+      status: 'unauthenticated'
+    })
 
     render (<SubscribButton />)
 
@@ -23,16 +25,51 @@ describe('SubscribeButton Component', () => {
   })
 
   test('signIn function é chamada se usuario nao tiver logado', () => {
+    const useSessionMocked = jest.mocked(useSession)
+    useSessionMocked.mockReturnValueOnce({
+      data: null,
+      status: 'unauthenticated'
+    })
+
     const signINMocked = jest.mocked(signIn)
 
-    const { debug } = render (<SubscribButton />)
+    render (<SubscribButton />)
 
     const subscribeButton = screen.getByText(/Subscribe now/i)
 
     fireEvent.click(subscribeButton)
 
     expect(signINMocked).toHaveBeenCalled()
-    debug()
+  })
+
+  test('usuario é redirecionado caso tenha inscricao ativa', () => {
+    const useRouterMocked = jest.mocked(useRouter)
+    const useSessionMocked = jest.mocked(useSession)
+    const pushMock = jest.fn()
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock
+    } as any)
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          email: "any_email@gmail.com",
+          name: "any_name"
+        },
+        userActiveSubscription: "active",
+        expires: "any_expires"
+      },
+      status: "authenticated"
+    })
+
+    render (<SubscribButton />)
+
+    const subscribeButton = screen.getByText(/Subscribe now/i)
+
+    fireEvent.click(subscribeButton)
+
+    expect(pushMock).toHaveBeenCalledWith('/posts')
   })
 })
 
